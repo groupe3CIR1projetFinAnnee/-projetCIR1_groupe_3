a035eb354ad43920fbd668bf923d777c47cea8b8
Katuro19 <151616494+Katuro19@users.noreply.github.com>
2024-06-14 15:05:24 +0200

    update person

--- person.h
+++ person.h
@@ -23,6 +23,21 @@ struct Person* createEmptyPerson(); //Return the "unknown person" with id of 0
 
 struct Person* createPerson(unsigned int id, char* firstname, char* lastname, char* birthday, unsigned int padreID, unsigned int madreID, char* region); //Will initiate a person, but not his/her parents.
 
+//Access
+unsigned int getID(struct Person* person);
+
+char* getFirstName(struct Person* person);
+char* getLastName(struct Person* person);
+
+unsigned int* getBirthday(struct Person* person); //Will return the birthday in an array, like this : [JJ,MM,AAAA]
+
+struct Person* getPadre(struct Person* person);
+struct Person* getMadre(struct Person* person);
+
+unsigned int getPadreID(struct Person* person);
+unsigned int getMadreID(struct Person* person);
+
+char* getRegion(struct Person* person);
 
 // Suppress
 void deletePerson(struct Person* person);
