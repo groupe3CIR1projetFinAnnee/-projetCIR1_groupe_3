5207783b08acde476829a51dafe91fbaba7bc071
Katuro19 <151616494+Katuro19@users.noreply.github.com>
2024-06-14 09:23:30 +0200

    update structs patch

--- gigaTree.h
+++ gigaTree.h
@@ -1,10 +1,14 @@
+#include <stdbool.h>
+
+#define LETTER_IN_ALPHABET 27
+
 #ifndef PROJET_GIGATREE_H
 #define PROJET_GIGATREE_H
 
 
 struct GigaTree{
     struct Person** person;
-    struct Regions* regionsTrie;
+    struct Region* regionsTrie;
     unsigned int birthday[12][31];
     struct Person* youngest;
     struct Person* oldest;
@@ -13,8 +17,20 @@ struct GigaTree{
     unsigned int numberPersons;
 };
 
+struct Region{
+    struct Region* regions[LETTER_IN_ALPHABET];
+    bool isRegion;
+    unsigned int births;
+};
 
 
+struct Person{
+    unsigned int id;
+    char* firstname;
+    char* lastname;
+    struct Person* padre;
+    struct Person* madre;
+};
 
 
 
