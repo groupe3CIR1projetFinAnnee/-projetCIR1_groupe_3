1eeed7cd1088c14d80fc70088595793a261bcbf5
Katuro19 <151616494+Katuro19@users.noreply.github.com>
2024-06-14 09:19:46 +0200

    struct

--- gigaTree.c
+++ gigaTree.c
@@ -1,5 +1,1 @@
-//
-// Created by perei on 14/06/2024.
-//
-
 #include "gigaTree.h"
--- gigaTree.h
+++ gigaTree.h
@@ -2,5 +2,26 @@
 #define PROJET_GIGATREE_H
 
 
+struct GigaTree{
+    struct Person**;
+    struct Regions*;
+    unsigned int birthday[12][31];
+    struct Person* youngest;
+    struct Person* oldest;
+    char* mostBirthsRegion;
+    unsigned int mostBirths;
+    unsigned int numberPersons;
+};
+
+
+
+
+
+
+
+
+
+
+
 
 #endif //PROJET_GIGATREE_H
