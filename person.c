dda5271e34e1975d76b87249c0f8241a6efcfda9
BananeRotative <93862669+BananeRotative@users.noreply.github.com>
2024-06-16 17:57:57 +0200

    Person documentation
    - Added clean documentation to Person
    - Added MALE and FEMALE constants

--- person.c
+++ person.c
@@ -36,6 +36,18 @@ struct Person* createEmptyPerson(){
 }
 */
 
+/**
+ * Create a new person, using the given parameters.
+ * Parents must be linked outside of this function when every people is created.
+ * @param id ID of this person
+ * @param firstname First name of this person
+ * @param lastname Last name of this person
+ * @param birthday Birthday of this person in a string. Format: DD/MM/YYYY
+ * @param padreID ID of this person's father
+ * @param madreID ID of this person's mother
+ * @param region Birth region of this person
+ * @return The created person
+ */
 struct Person* createPerson(unsigned int id, char* firstname, char* lastname, char* birthday, unsigned int padreID, unsigned int madreID, char* region){
     struct Person* person = malloc(sizeof(struct Person));
     if(person == NULL)
@@ -72,20 +84,41 @@ struct Person* createPerson(unsigned int id, char* firstname, char* lastname, ch
 }
 
 
-
+/**
+ * Get the ID of the given person
+ * @param person A person
+ * @return ID of the given person
+ */
 unsigned int getID(struct Person* person){
     return person->id;
 }
 
+/**
+ * Get the first name of the given person
+ * @param person A person
+ * @return First name of the given person
+ */
 char* getFirstName(struct Person* person){
     return person->firstname;
 }
+
+/**
+ * Get the last name of the given person
+ * @param person A person
+ * @return Last name of the given person
+ */
 char* getLastName(struct Person* person){
     return person->lastname;
 }
 
+
+/**
+ * Get the birth date of the given person
+ * @param person A person
+ * @return Birth date of the given person in an array. Format: [DD,MM,YYYY]
+ */
 unsigned int* getBirthday(struct Person* person){
-    unsigned int* birthday = malloc(sizeof(unsigned int) * 3); //3 int to save
+    unsigned int* birthday = malloc(sizeof(unsigned int) * 3); //3 ints to save
     if(birthday == NULL){
         return NULL;
     }
@@ -95,44 +128,100 @@ unsigned int* getBirthday(struct Person* person){
     return birthday;
 }
 
+
+/**
+ * Get the father of the given person
+ * @param person A person
+ * @return Father of the given person
+ */
 struct Person* getPadre(struct Person* person){
     return person->padre;
 }
+
+/**
+ * Get the mother of the given person
+ * @param person A person
+ * @return Mother of the given person
+ */
 struct Person* getMadre(struct Person* person){
     return person->madre;
 }
 
+/**
+ * Get ID of the father
+ * @param person A person
+ * @return ID of the father of person
+ */
 unsigned int getPadreID(struct Person* person){
     return person->padreID;
 }
+
+/**
+ * Get ID of the mother
+ * @param person A person
+ * @return ID of the mother of person
+ */
 unsigned int getMadreID(struct Person* person){
     return person->madreID;
 }
 
+/**
+ * Set the father of the given person.
+ * /!\ Also changes the known sex of the father
+ * @param person A person
+ * @param padre Father of the person
+ */
 void setPadre(struct Person* person, struct Person* padre){
     person->padre = padre;
     if(getID(padre) != 0)
-        person->padre->sex = 1; //We set the dad sex to 1 (= male) only if the padre is not the unknonw
+        person->padre->sex = MALE;  // We set the dad sex to male only if the padre is not the unknown person
 }
 
+/**
+ * Set the mother of the given person
+ * /!\ Also changes the known sex of the mother
+ * @param person A person
+ * @param madre Mother of the person
+ */
 void setMadre(struct Person* person, struct Person* madre){
     person->madre = madre;
     if(getID(madre) != 0)
-        person->madre->sex = 0; //We set the mom sex to 0 (= female) only if its not the unknown person
+        person->madre->sex = FEMALE; //We set the mom sex to female only if the madre is not the unknown person
 }
 
+/**
+ * Get birth region of the given person
+ * @param person A person
+ * @return Birth region of the given person
+ */
 char* getRegion(struct Person* person){
     return person->region;
 }
 
+/**
+ * Get gender of the given person
+ * Use MALE and FEMALE constants to compare the gender
+ * @param person A person
+ * @return gender of the given person
+ */
 bool getSex(struct Person* person){
     return person->sex;
 }
 
+/**
+ * Set gender of the given person
+ * @param person A person
+ * @param sex New gender of the person. Use MALE and FEMALE
+ */
 void setSex(struct Person* person, bool sex){
     person->sex = sex;
 }
 
+
+/**
+ * Delete the given person
+ * @param person The person to delete
+ */
 void deletePerson(struct Person** person){
     free((*person)->firstname);
     free((*person)->lastname);
@@ -142,6 +231,11 @@ void deletePerson(struct Person** person){
 }
 
 
+/**
+ * Split the given date string into a formatted array.
+ * @param birthday Birth date. Format: "DD/MM/YYYY"
+ * @return A array representing the birth date. Format: [day, month, year]
+ */
 unsigned int* splitBirthday(char* birthday){
 
     char* birthday_copy = strdup(birthday); // We copy the birthday here to be able to edit it with strtok
--- person.h
+++ person.h
@@ -20,43 +20,145 @@ struct Person{
     bool sex; //0 if female and 1 if male. Will be initied when another function will calculate the parents
 };
 
+#define MALE 1
+#define FEMALE 0
 
 // Creation & setup
+// TODO: delete useless commented code
 //struct Person* createEmptyPerson(); //Return the "unknown person" with id of 0 //Never used, and useless
 
-struct Person* createPerson(unsigned int id, char* firstname, char* lastname, char* birthday, unsigned int padreID, unsigned int madreID, char* region); //Will initiate a person, but not his/her parents.
+/**
+ * Create a new person, using the given parameters.
+ * Parents must be linked outside of this function when every people is created.
+ * @param id ID of this person
+ * @param firstname First name of this person
+ * @param lastname Last name of this person
+ * @param birthday Birthday of this person in a string. Format: DD/MM/YYYY
+ * @param padreID ID of this person's father
+ * @param madreID ID of this person's mother
+ * @param region Birth region of this person
+ * @return The created person
+ */
+struct Person* createPerson(unsigned int id, char* firstname, char* lastname, char* birthday, unsigned int padreID, unsigned int madreID, char* region);
 
 
 //Access
+
+/**
+ * Get the ID of the given person
+ * @param person A person
+ * @return ID of the given person
+ */
 unsigned int getID(struct Person* person);
 
+/**
+ * Get the first name of the given person
+ * @param person A person
+ * @return First name of the given person
+ */
 char* getFirstName(struct Person* person);
+
+/**
+ * Get the last name of the given person
+ * @param person A person
+ * @return Last name of the given person
+ */
 char* getLastName(struct Person* person);
 
-unsigned int* getBirthday(struct Person* person); //Will return the birthday in an array, like this : [JJ,MM,AAAA]
+// TODO: we can simplify return type from unsigned int* to unsigned int[3]
+/**
+ * Get the birth date of the given person
+ * @param person A person
+ * @return Birth date of the given person in an array. Format: [DD,MM,YYYY]
+ */
+unsigned int* getBirthday(struct Person* person);
 
+/**
+ * Get the father of the given person
+ * @param person A person
+ * @return Father of the given person
+ */
 struct Person* getPadre(struct Person* person);
+
+/**
+ * Get the mother of the given person
+ * @param person A person
+ * @return Mother of the given person
+ */
 struct Person* getMadre(struct Person* person);
 
-void setPadre(struct Person* person, struct Person* padre); //set the padre for a person. WARNING : WILL ALSO EDIT THE SEX OF THE DAD TO PUT A 1
-void setMadre(struct Person* person, struct Person* madre); //set the madre for a person. WARNING : WILL ALSO EDIT THE SEX OF THE MOM TO PUT A 0
+/**
+ * Set the father of the given person.
+ * /!\ Also changes the known sex of the father
+ * @param person A person
+ * @param padre Father of the person
+ */
+void setPadre(struct Person* person, struct Person* padre);
 
+/**
+ * Set the mother of the given person
+ * /!\ Also changes the known sex of the mother
+ * @param person A person
+ * @param madre Mother of the person
+ */
+void setMadre(struct Person* person, struct Person* madre);
 
+/**
+ * Get ID of the father
+ * @param person A person
+ * @return ID of the father of person
+ */
 unsigned int getPadreID(struct Person* person);
+
+/**
+ * Get ID of the mother
+ * @param person A person
+ * @return ID of the mother of person
+ */
 unsigned int getMadreID(struct Person* person);
 
+/**
+ * Get birth region of the given person
+ * @param person A person
+ * @return Birth region of the given person
+ */
 char* getRegion(struct Person* person);
 
+// TODO change return type bool to an enum, or use MALE/FEMALE (int)
+/**
+ * Get gender of the given person
+ * Use MALE and FEMALE constants to compare the gender
+ * @param person A person
+ * @return gender of the given person
+ */
 bool getSex(struct Person* person);
-void setSex(struct Person* person, bool sex); //Reminder : 0 = female and 1 = male
+
+// TODO change bool to int, or use an enum
+/**
+ * Set gender of the given person
+ * @param person A person
+ * @param sex New gender of the person. Use MALE and FEMALE
+ */
+void setSex(struct Person* person, bool sex);
 
 
-// Suppress
+// Deletion
+/**
+ * Delete the given person
+ * @param person The person to delete
+ */
 void deletePerson(struct Person** person);
 
 
-// OThers
-unsigned int* splitBirthday(char* birthday); //Transform the char birthday into an array of 3 elements. Dont forget to free it after use.
+// Others
+
+// TODO: use return type unsigned int[3] instead. Or even use 3 fonctions: getBirthDay, getBirthMonth, getBirthYear
+/**
+ * Split the given date string into a formatted array.
+ * @param birthday Birth date. Format: "DD/MM/YYYY"
+ * @return A array representing the birth date. Format: [day, month, year]
+ */
+unsigned int* splitBirthday(char* birthday);
 
 
 #endif //PROJET_PERSON_H
