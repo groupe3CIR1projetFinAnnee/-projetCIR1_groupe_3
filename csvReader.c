ce734cf5c7c84534dc0a71794d25ae1289292476
Katuro19 <151616494+Katuro19@users.noreply.github.com>
2024-06-18 09:15:04 +0200

    oui

--- csvReader.c
+++ csvReader.c
@@ -124,7 +124,7 @@ struct GigaTree* readCSV(char* filePath){ //The file path should look like this 
     gigaTree->people = people; //Setting up the array into the tree
     gigaTree->numberPersons = numberOfPerson; //Remember here !! the unknown person is counted, at the position 0, everytime.
     addParents(gigaTree); //the final boss
-    gigaTree->numFamilies = calculateNumberFamilies(gigaTree);
+    //gigaTree->numFamilies = calculateNumberFamilies(gigaTree);
     return gigaTree;
 }
 
